97eb36e3b6117a11e9e89cec1b3f1233cc6a0118
loriensleafs <pkloss@gmail.com>
2019-06-10 11:00:02 -0400

    Creates NavigationButton component for buttons in the Appbar.  I think I like this, but I haven't totally sold myself on this component.

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -6,19 +6,31 @@ import CartButton from './../CartButton';
 import Container from './../Container';
 import Divider from './../Divider';
 import Flex from './../Flex';
+import Text from './../Text';
 import FreeShippingButton from './../FreeShippingButton';
 import {
 	CartIcon,
+	ChevronUpIcon,
 	ChevronDownIcon,
+	ChevronLeftIcon,
+	ChevronRightIcon,
 	LocalShippingIcon,
 	ShoppingCartIcon,
 } from './../svgs';
 import logoImage from './logo.svg';
 
 /**
- * @name <AppBar>
- * @component
- * @public
+ * <ToggleButton> component.
+ */
+const NavigationButton = props => <Button {...props} />;
+NavigationButton.defaultProps = {
+	as      : Link,
+	color   : 'primary',
+	variant : 'contained',
+};
+
+/**
+ * <AppBar> component.
  */
 const AppBar = (props = {}) => (
 	<Box bg='primary.main' elevation={8} py={3.5} position='sticky' top='0px'>
@@ -30,63 +42,34 @@ const AppBar = (props = {}) => (
 					</Link>
 					<input style={{ width: '550px' }} />
 					<Flex ml={4}>
-						<Button
-							as={Link}
-							to='/help'
-							color='primary'
-							variant='contained'
-						>
+						<NavigationButton to='/help' arrow>
 							24/7 help
 							<ChevronDownIcon color='secondary' ml={2} />
-						</Button>
-						<Button
-							as={Link}
-							to='/account'
-							color='primary'
-							variant='contained'
-						>
+						</NavigationButton>
+						<NavigationButton to='/account' arrow>
 							your account
 							<ChevronDownIcon color='secondary' ml={2} />
-						</Button>
+						</NavigationButton>
 						<CartButton selected />
 					</Flex>
 				</Flex>
 				<Flex mt={3.5}>
 					<Flex flex={3} justifyContent='flex-start'>
-						<Button
-							color='primary'
-							variant='contained'
-							typography='h2'
-							fontWeight='bold'
-						>
-							shop by pet
-							<ChevronDownIcon color='secondary' ml={2} />
-						</Button>
-						<Button
-							as={Link}
-							to='/account'
-							color='primary'
-							variant='contained'
-						>
+						<NavigationButton>
+							<Text fontWeight='bold' typography='h2'>
+								shop by pet
+							</Text>
+						</NavigationButton>
+						<NavigationButton to='/account'>
 							shop by brand
-						</Button>
-						<Button
-							as={Link}
-							to='/account'
-							color='primary'
-							variant='contained'
-						>
+						</NavigationButton>
+						<NavigationButton to='/account'>
 							pharmacy
 							<ChevronDownIcon color='secondary' ml={2} />
-						</Button>
-						<Button
-							as={Link}
-							to='/account'
-							color='primary'
-							variant='contained'
-						>
+						</NavigationButton>
+						<NavigationButton to='/account'>
 							todays deals
-						</Button>
+						</NavigationButton>
 					</Flex>
 					<Flex flex={2} justifyContent='flex-end'>
 						<FreeShippingButton />
